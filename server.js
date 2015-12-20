af3e3915b7301970831efbd63c2e8820a7cda1dd
Andrey F. Kupreychik <foxel@quickfox.ru>
2015-12-20 16:43:56 +0600

    Styling

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ server.set('view engine', 'ejs');
 server.get('/', app.new);
 
 server.get('/help', function (req, res) {
-    res.render('pages/help');
+    res.render('pages/help', {url: req.url});
 });
 
 server.get('/upload', function (req, res) {
-    res.render('pages/upload');
+    res.render('pages/upload', {url: req.url});
 });
 server.post('/upload', app.upload);
 server.post('/update/doc\\d+/\\d+/[A-Za-z]+\\d+', app.update);
@@ -28,6 +28,7 @@ server.get('/download/doc\\d+', app.download);
 server.get('/json/doc\\d+', app.json);
 server.get('/doc\\d+', function(req, res) {
     res.render('pages/doc', {
+        url: req.url,
         documentId: path.basename(req.url)
     });
 });
