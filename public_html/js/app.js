2862ef8ad0c72ca5587ada8c012d928988d645c9
Andrey F. Kupreychik <foxel@quickfox.ru>
2015-12-20 15:15:24 +0600

    Fixed for test step 1

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -11,11 +11,13 @@ App.prototype = {
         console.log(this);
         $.when(
             this.loadResource('/json/'+this.docId),
-            this.loadTemplate('grid')
+            this.loadTemplate('grid'),
+            this.loadTemplate('modal')
         )
-        .done(_.bind(function(data, gridTemplate) {
+        .done(_.bind(function(data, gridTemplate, modalTemplate) {
             this.sheets = data;
             this.gridTemplate = gridTemplate;
+            this.modalTemplate = modalTemplate;
             this.render();
 
         }, this));
@@ -26,7 +28,36 @@ App.prototype = {
             active: 0,
             app: this
         }));
+
+        this.$el.delegate('td.data-cell', 'dblclick', _.bind(function(e) {
+            e.preventDefault();
+            var $el = $(e.currentTarget),
+                col = $el.data('col'),
+                row = $el.data('row'),
+                sheetId = $el.closest('table').data('sheet');
+            this.openEditor(sheetId, col, row);
+            return false;
+        }, this));
     },
+    openEditor: function(sheetId, col, row) {
+        var sheet = this.sheets[sheetId],
+            cell = (sheet.cells[col] && sheet.cells[col][row])
+            ? sheet.cells[col][row]
+            : {value: '\'', computed: ''};
+        var modal = $(this.modalTemplate({
+            cell: cell,
+            app: this
+        })).modal();
+
+        modal.find('.btn-primary').click(_.bind(function() {
+            cell.value = modal.find('input[name="value"]').val();
+            sheet.cells[col] || (sheet.cells[col] = {});
+            sheet.cells[col][row] = cell;
+            this.sendUpdate()
+        }, this))
+    },
+
+
     loadResource: function(url) {
         return $.get(url).then(function(data) {
             return data;
@@ -62,10 +93,23 @@ App.prototype = {
         return digits;
     },
     encodeDisplayed: function (value) {
-        if (isNaN(value)) {
-            return '\'' + value;
+        if (String(value).trim() == '') {
+            return '';
+        } else if (isNaN(value)) {
+            return value;
         } else {
             return String(+Number(value).toFixed(8));
+        }
+    },
+    encodeValue: function (value) {
+        if (!String(value).trim().length) {
+            return '';
+        } else if (value[0] == '=') {
+            return value;
+        } else if (value[0] == '\'') {
+            return String(value.substr(1));
+        } else {
+            return isNaN(value) ? '' : parseFloat(value);
         }
     }
 };
